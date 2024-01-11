aead07ba9b97b8bef623b05501df1e600f60dad0
avbravo <avbravo@avbravo>
2024-01-11 16:13:11 -0500

    1.0.1

--- src/main/java/com/jmoordbcoreencripter/jmoordbencripter/Encryptor.java
+++ src/main/java/com/jmoordbcoreencripter/jmoordbencripter/Encryptor.java
@@ -15,53 +15,56 @@ import javax.crypto.spec.SecretKeySpec;
 
 /**
  *
- * @author Lokesh Gupta
- * Email: howtodoinjava@gmail.com
- * Twitter: @HowToDoInJava
+ * @author Lokesh Gupta Email: howtodoinjava@gmail.com Twitter: @HowToDoInJava
  * https://howtodoinjava.com/java/java-security/java-aes-encryption-example/
  */
 public class Encryptor {
-private static SecretKeySpec secretKey;
-  private static byte[] key;
+    private static Long count=0L;
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
 
-  public static void setKey(final String myKey) {
-    MessageDigest sha = null;
-    try {
-      key = myKey.getBytes("UTF-8");
-      sha = MessageDigest.getInstance("SHA-1");
-      key = sha.digest(key);
-      key = Arrays.copyOf(key, 16);
-      secretKey = new SecretKeySpec(key, "AES");
-    } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-      e.printStackTrace();
+    public static void setKey(final String myKey) {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  public static String encrypt(final String strToEncrypt, final String secret) {
-    try {
-      setKey(secret);
-      Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-      cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-      return Base64.getEncoder()
-        .encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
-    } catch (Exception e) {
-      System.out.println("Encryptor.encrypt()->[Error while encrypting]: " + e.toString());
+    public static String encrypt(final String strToEncrypt, final String secret) {
+        try {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder()
+                    .encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        } catch (Exception e) {
+            System.out.println("Encryptor.encrypt()->[Error while encrypting]: " + e.toString());
+        }
+        return null;
     }
-    return null;
-  }
 
-  public static String decrypt(final String strToDecrypt, final String secret) {
-    try {
-      setKey(secret);
-      Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-      cipher.init(Cipher.DECRYPT_MODE, secretKey);
-      return new String(cipher.doFinal(Base64.getDecoder()
-        .decode(strToDecrypt)));
-    } catch (Exception e) {
-        System.out.println("\t[test] secretKey"+secretKey);
-        System.out.println("\t[test] strToDecrypt"+strToDecrypt);
-      System.out.println("Encryptor.decrypt()-> [Error while dencrypting]: " + e.toString());
+    public static String decrypt(final String strToDecrypt, final String secret, final String nameOfMethod) {
+        try {
+            count++;
+            System.out.println("\tþ[decrypt count] "+count);
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder()
+                    .decode(strToDecrypt)));
+        } catch (Exception e) {
+            System.out.println("\t[test] secretKey" + secretKey);
+            System.out.println("\t[test] strToDecrypt" + strToDecrypt);
+            System.out.println("\t[test] nameOfMethod" + nameOfMethod);
+          
+            System.out.println("Encryptor.decrypt()-> [Error while dencrypting]: " + e.toString());
+        }
+        return null;
     }
-    return null;
-  }
 }
